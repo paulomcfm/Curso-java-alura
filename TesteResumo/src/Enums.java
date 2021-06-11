f5995f6323cdd10985003ea6241f1bb4b1e387de
paulomcfm <paulomcfm@gmail.com>
2021-06-11 10:25:07 -0300

    Update Enums.java

--- TesteResumo/src/Enums.java
+++ TesteResumo/src/Enums.java
@@ -5,11 +5,13 @@ public class Enums {
 		
 		Prioridade pMin = Prioridade.MIN;
 		Prioridade pMax = Prioridade.MAX;
-		
+		Prioridade pNormal = Prioridade.NORMAL;
 		System.out.println(pMin.name());
 		System.out.println(pMin.ordinal());
 		System.out.println(pMax.name());
 		System.out.println(pMax.ordinal());
+		System.out.println(pNormal.name());
+		System.out.println(pNormal.ordinal());
 	}
 
 }
