5832948126cfdbbac463dab57a65aaf08eb1ba15
paulomcfm <paulomcfm@gmail.com>
2021-06-15 17:47:54 -0300

    Bibliotecas

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,30 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita2 {
+
+	public static void main(String[] args) throws IOException {
+
+		
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("sdasdaasd asd asd adsd");
+		
+		bw.close();
+		
+	}
+
+}
